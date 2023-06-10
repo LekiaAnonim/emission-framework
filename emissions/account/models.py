b279c1be6fea83561e076829d69ab52f726ab618
Lekia Prosper <63610232+LekiaAnonim@users.noreply.github.com>
2023-06-10 23:22:44 +0100

    create django apps, account and flare_emission

---
+++ emissions/account/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+
+# import user from django
+from django.contrib.auth.models import User
+
+# import slug field from django
+from django.utils.text import slugify
+
+# Create your models here.
+class Profile(models.Model):
+    company_name = models.CharField(max_length=500, null=True)
+    company_logo = models.ImageField(null=True, blank=True)
+    root_admin = models.OneToOneField(User, on_delete=models.SET_NULL)
+    industry_category = models.OneToOneField(IndustryCategory, on_delete=models.SET_NULL)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    slug = models.SlugField(null=True,  max_length=500)
+
+    def __str__(self):
+        return self.company_name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.company_name, allow_unicode=True)
+        super(Profile, self).save(*args, **kwargs)
+
+class IndustryCategory(models.Model):
+    industry = models.CharField(max_length=500, null=True, blank=True)
+
+    class Meta:
+        # create a verbose name meta class
+        verbose_name = 'Industry Category'
+        verbose_name_plural = 'Industry Categories'
+
+    # create a str method that will return industry attribute as a string
+    def __str__(self):
+        return self.industry
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.industry, allow_unicode=True)
+        super(IndustryCategory, self).save(*args, **kwargs)
+    
+
+
+
+
+class Account(models.Model):
+    company_profie = models.OneToOneField(Profile, on_delete=models.SET_NULL)
+    date_create = models.DateField(auto_now=True)
+    subscription_plan = models.ForeignKey(SubscriptionPlan, on_delete=models.CASCADE)
+    add_users = models.ManyToManyField(User)
+
+
+class SubscriptioPlan(models.Model):
+    user_limit = models.IntegerField(null=True, blank=True)
+    start_date = models.DateTimeField(auto_now=True)
+    duration = models.DurationField(null=True, blank=True)
+    subscription_package = models.ManyToManyField(SubscriptioPackage)
+
+class SubscriptionPackage(models.Model):
+    packages = models.ManyToManyField(EmissionPackage)
+
+class EmissionPackage(models.Model):
+    emission_type = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.emission_type
